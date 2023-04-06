db18e29607df5e568d127f083996d728c2ddea1e
jgmapus <126249165+jgmapus@users.noreply.github.com>
2023-04-06 17:28:54 +0900

    Create hap.c

---
+++ C_6/hap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int i, n, sum=0;
+
+    printf("정수를 입력하시오:");
+    scanf("%d", &n);
+
+    while (i<=n)
+    {
+        sum += i;
+        i++;
+    }
+
+    printf("1부터 %d까지의 합은 %d입니다\n", n, sum);
+    return 0;
+}
