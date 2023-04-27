ed819123c7f83a5ecc1b4a0fd196caa9a9929e5c
jgmapus <126249165+jgmapus@users.noreply.github.com>
2023-04-27 18:50:03 +0900

    Create wkfysize.c

---
+++ C_3/wkfysize.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+int x;
+printf("변수x의 크기: %d\n", sizeof(x));
+printf("char형의 크기: %d\n", sizeof(char));
+printf("int형의 크기: %d\n", sizeof(int));
+printf("short형의 크기: %d\n", sizeof(short));
+printf("long형의 크기: %d\n", sizeof(long));
+printf("float형의 크기: %d\n", sizeof(float));
+printf("double형의 크기: %d\n", sizeof(double));
+return 0;
+}
