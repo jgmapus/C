ba1d428b7d821bfd803a75bb7e1ef92a95961fad
jgmapus <126249165+jgmapus@users.noreply.github.com>
2023-04-27 18:50:22 +0900

    Create wjdtntkdtn.c

---
+++ C_3/wjdtntkdtn.c
@@ -0,0 +1,10 @@
+/* 정수 상수 프로그램*/ 
+#include <stdio.h>
+int main(void)
+{ 
+int x = 10; // 10은 10진수이고 int형이고 값은 십진수로 10이다. int y = 010; // 010은 8진수이고 int형이고 값은 십진수로 8이다. 
+int z = 0x10; // 010은 16진수이고 int형이고 값은 십진수로 16이다. printf("x = %d", x);
+printf("y = %d", y);
+printf("z = %d", z);
+return 0; 
+}
