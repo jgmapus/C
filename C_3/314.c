0674cecce63a3a9e80b6916bd09320c889584397
jgmapus <126249165+jgmapus@users.noreply.github.com>
2023-04-27 18:49:46 +0900

    Create 314.c

---
+++ C_3/314.c
@@ -0,0 +1,12 @@
+/* 원의 면적을 계산하는 프로그램 */
+#include <stdio.h>
+int main(void)
+{
+float radius; // 원의 반지름
+float area; // 원의 면적
+printf("원의 면적을 입력하시요:");
+scanf("%f", &radius);
+area = 3.141592 * radius * radius;
+printf("원의 면적: %f \n", area);
+return 0;
+}
