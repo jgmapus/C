444095a41af363236f997fb8d043a2a989aa504e
jgmapus <126249165+jgmapus@users.noreply.github.com>
2023-04-27 18:48:53 +0900

    Update rkrkatmdwp.c

--- C_3/rkrkatmdwp.c
+++ C_3/rkrkatmdwp.c
@@ -11,3 +11,9 @@ sum = x + y; // 변수 sum에 (x+y)의 결과를 저장
 diff = x - y; // 변수 diff에 (x-y)의 결과를 저장
 mul = x * y; // 변수 mul에 (x*y)의 결과를 저장
 div = x / y; // 변수 div에 (x/y)의 결과를
+printf("두수의 합: %d\n", sum); // 변수 sum의 값을 화면에 출력
+printf("두수의 차: %d\n", diff);// 변수 diff의 값을 화면에 출력
+printf("두수의 곱: %d\n", mul); // 변수 mul의 값을 화면에 출력
+printf("두수의 몫: %d\n", div); // 변수 div의 값을 화면에 출력
+return 0;
+}
